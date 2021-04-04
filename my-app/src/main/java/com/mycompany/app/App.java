2e37a6a57a66c4cfc56d5810b627428e662464cd
g9itnaresh <g9itnaresh@gmail.com>
2021-04-04 13:49:18 +0000

    added

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!   this is test app" );
+        System.out.println( "Hello World! \n  this is test app" );
 
 /**
         System.out.println("A NullPointerException is a drag...");
